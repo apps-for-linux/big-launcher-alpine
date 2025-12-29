feff223ac3a8abe581bbd30944ae0e1be446eb5e
DrSheppard <60893791+Drsheppard01@users.noreply.github.com>
2025-12-29 22:03:39 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,6 +11,6 @@ echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> ./alp/root/etc/apk/r
 sudo chroot ./alp/root/ /bin/ash -l -c "apk update && apk upgrade && apk add sdl3 sdl3_ttf sdl3_image make cmake libarchive harfbuzz spdlog fmt libxml2 inih"
 sudo git clone https://github.com/complexlogic/big-launcher.git
 cd big-launcher
-sudo cmake -B
+sudo cmake ..
 sudo make
 ARCH=x86_64 VERSION=clean ./appimagetool -n ./alp/
