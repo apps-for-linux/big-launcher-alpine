54776eaf16f6a1d848042d3bf38c3ba56fa6a5dd
DrSheppard <60893791+Drsheppard01@users.noreply.github.com>
2025-12-31 02:04:03 +0300

    update build.sh

--- build.sh
+++ build.sh
@@ -32,7 +32,7 @@ exit"
 cp ${GITHUB_WORKSPACE}/icon.png ./alp/
 cp ${GITHUB_WORKSPACE}/Big-Launcher.desktop ./alp/
 cp ${GITHUB_WORKSPACE}/AppRun ./alp/
-chmod a+x ./alp/AppRun \
+chmod a+x ./alp/AppRun
 sudo umount -lf ./alp/root/proc
 sudo umount -lf ./alp/root/sys
 sudo umount -lf ./alp/root/dev
